d2c5562b7a62cb4a41a1fcda69012c920a42b6c4
angeldev-23 <ojoyangel14@gmail.com>
2024-10-20 14:46:24 +0800

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,8 @@ function About() {
     <div className="container">
       <h1>About Me</h1>
       <p>I'm 3rd Year Collage, I love reading story, drawing, travel, and also watching k-dramas. 
-        My Favorite Color is Pink </p>
+        I have experience working at Lazada, where I was involved in the sorting process. This role gave me hands-on experience in 
+        organizing and handling products efficiently. My goal is to achieve my dreams by working hard, I am determined to turn my ambitions into reality through dedication and perseverance </p>
     </div>
   );
 }
