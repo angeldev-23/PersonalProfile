47d6b4731ac22e6f0cc5af17e6f7be95b1753c4f
angeldev-23 <ojoyangel14@gmail.com>
2024-10-20 14:56:12 +0800

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,8 @@ function Home() {
     <div className="container">
       <h1>Welcome to My Profile!</h1>
       <p>I a'm Angel B. Ojoy, taking a computer science in Pamantasan ng Cabuyao. 
-        The career i want someday is to be a programmer designer</p>
+        The career i want someday is to be a programmer designer.My goal is to achieve my dreams by working hard,
+        I am determined to turn my ambitions into reality through dedication and perseverance</p>
     </div>
   );
 }
